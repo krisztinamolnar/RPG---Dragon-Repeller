6ac9b8ada21e5ff3544dcfe3cfaafa762ba60cd9
krisztinamolnar <krisztina.molnar.mail@gmail.com>
2024-02-11 02:55:09 +0100

    Added "buy health" functionality

--- src/script.js
+++ src/script.js
@@ -68,7 +68,16 @@ function goCave() {
 function fightDragon() {
   console.log("Fighting dragon.");
 }
-function buyHealth() {}
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 function buyWeapon() {}
 function fightSlime() {}
 function fightBeast() {}
